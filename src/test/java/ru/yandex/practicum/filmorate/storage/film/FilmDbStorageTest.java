5fc889895c0e2b1c9bb51ebf89747c6402195130
Aleksey Seledcov <alex-ozu@yandex.ru>
2025-08-22 13:05:44 +0300

    Fixed the tests

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
@@ -12,6 +12,8 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.like.LikeStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
@@ -25,10 +27,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({FilmDbStorage.class})
 class FilmDbStorageTest {
-    private final JdbcTemplate jdbcTemplate;
+    private final FilmService filmService;
     private final FilmDbStorage filmStorage;
     private final LikeStorage likeStorage;
-    private final UserDbStorage userDbStorage;
+    private final UserService userService;
     private Film newFilm1;
     private Film newFilm2;
     private User newUser1;
@@ -72,7 +74,7 @@ class FilmDbStorageTest {
     @Test
     void addFilm() {
         Film actualFilm = newFilm1;
-        Film expectedFilm = filmStorage.addFilm(newFilm1);
+        Film expectedFilm = filmService.addFilm(newFilm1);
         assertEquals(expectedFilm.getName(), actualFilm.getName());
         assertEquals(expectedFilm.getDescription(), actualFilm.getDescription());
         assertEquals(expectedFilm.getReleaseDate(), actualFilm.getReleaseDate());
@@ -92,28 +94,28 @@ class FilmDbStorageTest {
 
     @Test
     void getPopularFilmList() {
-        newFilm1 = filmStorage.addFilm(newFilm1);
-        newFilm2 = filmStorage.addFilm(newFilm2);
-        userDbStorage.addUser(newUser1);
-        userDbStorage.addUser(newUser2);
-        likeStorage.putLike(1,newFilm1.getId());
-        likeStorage.putLike(1,newFilm2.getId());
-        likeStorage.putLike(2,newFilm2.getId());
-        List<Film> popularFilms = filmStorage.getPopularFilmList(3);
+        newFilm1 = filmService.addFilm(newFilm1);
+        newFilm2 = filmService.addFilm(newFilm2);
+        userService.addUser(newUser1);
+        userService.addUser(newUser2);
+        likeStorage.putLike(1, newFilm1.getId());
+        likeStorage.putLike(1, newFilm2.getId());
+        likeStorage.putLike(2, newFilm2.getId());
+        List<Film> popularFilms = filmService.getPopularFilmList(3);
         assertTrue(popularFilms.get(0).getLikesCount() > popularFilms.get(1).getLikesCount());
     }
 
     @Test
     void getFilm() {
         Film actualFilm = newFilm1;
-        Film expectedFilm = filmStorage.addFilm(actualFilm);
+        Film expectedFilm = filmService.addFilm(actualFilm);
         assertEquals(expectedFilm, actualFilm);
     }
 
     @Test
     void containsFilmById() {
-        Film film = filmStorage.addFilm(newFilm1);
-        filmStorage.addFilm(newFilm2);
+        Film film = filmService.addFilm(newFilm1);
+        filmService.addFilm(newFilm2);
         assertTrue(filmStorage.containsFilmById(film.getId()));
         assertTrue(filmStorage.containsFilmByName(film.getName()));
 
